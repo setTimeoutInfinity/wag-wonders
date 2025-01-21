14d77c13fc91562e164d8855ac27da5530880ccf
setTimeoutInfinity <dan.lakzian@gmail.com>
2025-01-21 15:21:57 +1100

    feat: add node adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
@@ -12,4 +14,8 @@ export default defineConfig({
       mode: "inline",
     },
   },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
